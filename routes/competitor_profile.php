987416b69c4cd7054e2637f1315cf2aa7e368bfd
it17184922-BalasooriyaSA <it17184922@my.sliit.lk>
2022-04-28 23:58:58 +0530

    Initial commit

---
+++ routes/competitor_profile.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Online Voting System - Competitor Profile</title>
+    </head>
+    <body>
+        <button>Back</button>
+        <button>Logout</button>
+        <h1>Online Voting System</h1>
+        <hr>
+    </body>
+</html>
