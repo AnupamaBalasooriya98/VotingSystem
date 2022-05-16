b99bc28701c07148d1382258a921f11b77268fd0
AnupamaBalasooriya_98 <58579939+AnupamaBalasooriya98@users.noreply.github.com>
2022-05-16 01:33:15 -0700

    Upload database file

---
+++ online_voting_system.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 16, 2022 at 10:32 AM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 7.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `online_voting_system`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `competitor_register`
+--
+
+CREATE TABLE `competitor_register` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(20) NOT NULL,
+  `last_name` varchar(20) NOT NULL,
+  `nic` varchar(12) NOT NULL,
+  `age` int(11) NOT NULL,
+  `mobile_number` int(10) NOT NULL,
+  `home_address` varchar(100) NOT NULL,
+  `dp` varchar(255) NOT NULL,
+  `password` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `competitor_register`
+--
+
+INSERT INTO `competitor_register` (`id`, `first_name`, `last_name`, `nic`, `age`, `mobile_number`, `home_address`, `dp`, `password`) VALUES
+(4, 'anupama', 'balasooriya', '123456v', 24, 123456, 'jsgdfjsdfkh', '', '123456v');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `competitor_register`
+--
+ALTER TABLE `competitor_register`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `competitor_register`
+--
+ALTER TABLE `competitor_register`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
