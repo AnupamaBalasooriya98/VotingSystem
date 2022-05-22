7a7c10a13fd6d067719a2e51c93266241044e6cb
it17184922-BalasooriyaSA <it17184922@my.sliit.lk>
2022-05-22 14:43:30 +0530

    Add 'Judgers CRUD'

--- php/admin_register.php
+++ php/admin_register.php
@@ -4,14 +4,14 @@
 
     $username = $_POST['username'];
     $password = $_POST['password'];
-    $confpassword = $_POST['confPassword'];
+    $connectfpassword = $_POST['confPassword'];
     $type = $_POST['type'];
     $image = $_FILES['photo']['name'];
     $tmp_name = $_FILES['photo']['tmp_name'];
 
     move_uploaded_file($tmp_name, "../images/$image");
 
-    if ($password == $confpassword) {
+    if ($password == $connectfpassword) {
         $insert = mysqli_query($connect, "INSERT INTO admins (username, password, image) VALUES ('$username', '$password', '$image')");
         $login = mysqli_query($connect, "INSERT INTO login (username, password, type) VALUES ('$username', '$password', '$type')");
 
