5dbb6456774067fc7de5bd8bdf2487e34ecf3895
it17184922-BalasooriyaSA <it17184922@my.sliit.lk>
2022-05-22 14:13:43 +0530

    Fix edit and register functions

--- php/admin_register.php
+++ php/admin_register.php
@@ -5,18 +5,20 @@
     $username = $_POST['username'];
     $password = $_POST['password'];
     $confpassword = $_POST['confPassword'];
+    $type = $_POST['type'];
     $image = $_FILES['photo']['name'];
     $tmp_name = $_FILES['photo']['tmp_name'];
 
     move_uploaded_file($tmp_name, "../images/$image");
 
-    // if ($password == $confpassword) {
+    if ($password == $confpassword) {
         $insert = mysqli_query($connect, "INSERT INTO admins (username, password, image) VALUES ('$username', '$password', '$image')");
+        $login = mysqli_query($connect, "INSERT INTO login (username, password, type) VALUES ('$username', '$password', '$type')");
 
-        /*if ($insert) {
+        if ($insert && $login) {
             echo "
                 <script>
-                    alert('Registration successfull!!');
+                    alert('Registration successfull!! Login created!!');
                     window.location = '../html/admin_register.html'
                 </script>
             ";
@@ -28,15 +30,15 @@
                     window.location = '../html/admin_register.html'
                 </script>
             ";
-        }*/
-    // }
-    // else {
-    //     echo "
-    //         <script>
-    //             alert('Confirm password should be equal to password!!');
-    //             window.location = '../html/admin_register.html'
-    //         </script>
-    //     ";
-    // }
+        }
+    }
+    else {
+        echo "
+            <script>
+                alert('Passwords do not match!!');
+                window.location = '../html/admin_register.html'
+            </script>
+        ";
+    }
 
 ?>
