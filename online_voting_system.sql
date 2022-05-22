47ad264d645affa3f00cc66401120bf1d6cb11fe
AnupamaBalasooriya_98 <58579939+AnupamaBalasooriya98@users.noreply.github.com>
2022-05-22 02:14:23 -0700

    Update sql

--- online_voting_system.sql
+++ online_voting_system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 21, 2022 at 06:44 PM
+-- Generation Time: May 22, 2022 at 11:13 AM
 -- Server version: 10.1.31-MariaDB
 -- PHP Version: 7.2.4
 
@@ -40,9 +40,25 @@ CREATE TABLE `admins` (
 --
 
 INSERT INTO `admins` (`id`, `username`, `password`, `image`) VALUES
-(1, 'admin', 'admin', '92-920036_nature-top-whatsapp-dp.jpg'),
-(3, '', '', ''),
-(4, '', '', '');
+(6, 'admin123', 'admin123', 'Natsume-gakuen-alice-20827908-600-450 (1).jpg'),
+(7, 'admin321', 'admin321', 'original-25020-1453944978-3.jpg'),
+(34, '', '', ''),
+(35, 'admin', 'admin', 'Natsume-ans-Mikan-Sakura-natsume-hyuuga-13321757-400-250.jpg'),
+(36, '123456', '123456', '92-920036_nature-top-whatsapp-dp.jpg'),
+(37, 'admin', 'admin', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+CREATE TABLE `contact` (
+  `ID` int(11) NOT NULL,
+  `Name` varchar(45) DEFAULT NULL,
+  `Email` varchar(45) DEFAULT NULL,
+  `Msg` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -53,15 +69,40 @@ INSERT INTO `admins` (`id`, `username`, `password`, `image`) VALUES
 CREATE TABLE `login` (
   `id` int(11) NOT NULL,
   `username` varchar(10) NOT NULL,
-  `password` varchar(10) NOT NULL
+  `password` varchar(10) NOT NULL,
+  `type` varchar(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `login`
 --
 
-INSERT INTO `login` (`id`, `username`, `password`) VALUES
-(1, 'admin', 'admin');
+INSERT INTO `login` (`id`, `username`, `password`, `type`) VALUES
+(1, 'admin', 'admin', 'admin'),
+(3, '', '', 'admin'),
+(4, 'admin', 'admin', 'admin'),
+(5, '123456', '123456', 'admin'),
+(6, 'admin', 'admin', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reg`
+--
+
+CREATE TABLE `reg` (
+  `ID` int(11) NOT NULL,
+  `FirstName` varchar(45) NOT NULL,
+  `LastName` varchar(45) NOT NULL,
+  `Address` varchar(45) NOT NULL,
+  `Email` varchar(45) NOT NULL,
+  `Phone` int(10) NOT NULL,
+  `NICNumber` varchar(45) NOT NULL,
+  `DateOfBirth` date NOT NULL,
+  `Gender` varchar(45) NOT NULL,
+  `JobTitle` varchar(45) NOT NULL,
+  `Password` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexes for dumped tables
@@ -74,10 +115,22 @@ ALTER TABLE `admins`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`ID`);
+
+--
 -- Indexes for table `login`
 --
 ALTER TABLE `login`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `reg`
+--
+ALTER TABLE `reg`
+  ADD PRIMARY KEY (`ID`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -87,13 +140,25 @@ ALTER TABLE `login`
 -- AUTO_INCREMENT for table `admins`
 --
 ALTER TABLE `admins`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+
+--
+-- AUTO_INCREMENT for table `contact`
+--
+ALTER TABLE `contact`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `login`
 --
 ALTER TABLE `login`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `reg`
+--
+ALTER TABLE `reg`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
